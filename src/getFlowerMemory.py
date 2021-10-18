8286dc3b28229b71d069c8c193835712b5ef5e9e
LostLogia4 <hayate04@live.com>
2021-10-18 21:23:45 +0800

    Update getFlowerMemory.py

--- src/getFlowerMemory.py
+++ src/getFlowerMemory.py
@@ -64,25 +64,41 @@ class DownloadImage(object):
 		my.iconFrame(flowerKnightEntry.image_id0,flowerKnightEntry.rarity,flowerKnightEntry.type)
 		
 	def getFlowerMemoryImage(my, flowerMemoryEntry):
-                
-		IMG_FM_ICON  = ImageClass(["flower_memory/general/icon","fm_icon_","memory_icon_"])
-		IMG_FM_IMAGE = ImageClass(["flower_memory/general/main_l","fm_main_l_","memory_wallpaper_"])
-		IMG_FM_TITLE = ImageClass(["flower_memory/general/name_l","fm_name_l_","memory_title_"])
+		
+		IMG_FM_ICON  = ImageClass(["flower_memory/general/icon","fm_icon_","Fm_icon_"])
+		IMG_FM_ICON2 = ImageClass(["flower_memory/general/icon_half","fm_icon_half_","Fm_icon_half_"])
+		IMG_FM_IMAGE = ImageClass(["flower_memory/general/main_l","fm_main_l_","Fm_main_l_"])
+		IMG_FM_TITLE = ImageClass(["flower_memory/general/name_l","fm_name_l_","Fm_title_"])
 		
 		my.downloadCharaImage(flowerMemoryEntry['itemId'],IMG_FM_ICON)
+		my.downloadCharaImage(flowerMemoryEntry['itemId'],IMG_FM_ICON2)
 		my.downloadCharaImage(flowerMemoryEntry['itemId'],IMG_FM_IMAGE)
 		my.downloadCharaImage(flowerMemoryEntry['itemId'],IMG_FM_TITLE)
 
 		my.iconFrame(flowerMemoryEntry['itemId'], IMG_FM_ICON, flowerMemoryEntry['rarity'])
+		my.iconHalfFrame(flowerMemoryEntry['itemId'], IMG_FM_ICON2, flowerMemoryEntry['rarity'])
 
 		fm_image_filename = os.path.join(ASSET_DIRECTORY,"{0}{1}.png".format(IMG_FM_IMAGE.imgSaveName,flowerMemoryEntry['itemId']))
+		fm_image_output_filename = os.path.join(ASSET_DIRECTORY,"{0}{1}.png".format("Fm_wallpaper_",flowerMemoryEntry['itemId']))
 		fm_title_filename = os.path.join(ASSET_DIRECTORY,"{0}{1}.png".format(IMG_FM_TITLE.imgSaveName,flowerMemoryEntry['itemId']))
 		try:
-			IconMerger.get_framed_memory(fm_image_filename, fm_title_filename, fm_image_filename, int(flowerMemoryEntry['rarity']))
+			IconMerger.get_framed_memory(fm_image_filename, fm_title_filename, fm_image_output_filename, int(flowerMemoryEntry['rarity']))
 		except Exception as e:
 			print(e)
 		else:
-			os.remove(os.path.join(ASSET_DIRECTORY,"memory_title_{0}.png".format(flowerMemoryEntry['itemId'])))
+			print("Successfully framed {0}{1}.png".format("Fm_wallpaper_",flowerMemoryEntry['itemId']))
+			#os.remove(IMG_FM_TITLE.imgSaveName)))
+
+		fm_image_filename = os.path.join(ASSET_DIRECTORY,"{0}{1}.png".format(IMG_FM_IMAGE.imgSaveName,flowerMemoryEntry['itemId']))
+		fm_image_output_filename = os.path.join(ASSET_DIRECTORY,"{0}{1}.png".format("Fm_wallpaper_",flowerMemoryEntry['itemId']))
+		fm_title_filename = os.path.join(ASSET_DIRECTORY,"{0}{1}.png".format(IMG_FM_TITLE.imgSaveName,flowerMemoryEntry['itemId']))
+		try:
+			IconMerger.get_framed_memory(fm_image_filename, fm_title_filename, fm_image_output_filename, int(flowerMemoryEntry['rarity']))
+		except Exception as e:
+			print(e)
+		else:
+			print("Successfully framed {0}{1}.png".format("Fm_wallpaper_",flowerMemoryEntry['itemId']))
+			os.remove(IMG_FM_TITLE.imgSaveName)))
 		
 	def getFilenameHash(my, predicate, inputID):
 		#Calculate the hashed filename of the character images.
@@ -128,10 +144,21 @@ class DownloadImage(object):
 			else:
 				IconMerger.get_framed_icon(filepath,filepath,int(rarity),int(attribute))
 		except Exception as e:
-			print("Unable to frame {0}.png".format(inputID))
+			print("Unable to frame {0}{1}.png".format(image_type.imgSaveName,inputID))
 			print(e)
 		else:
-			print("Successfully framed {0}.png".format(inputID))
+			print("Successfully framed {0}{1}.png".format(image_type.imgSaveName,inputID))
+
+	def iconHalfFrame(my,inputID,image_type,rarity):
+		filepath = os.path.join(ASSET_DIRECTORY, "{0}{1}.png".format(image_type.imgSaveName,inputID))
+		
+		try:
+			IconMerger.get_framed_halficon(filepath,filepath,int(rarity))
+		except Exception as e:
+			print("Unable to frame {0}{1}.png".format(image_type.imgSaveName,inputID))
+			print(e)
+		else:
+			print("Successfully framed {0}{1}.png".format(image_type.imgSaveName,inputID))
 
 class MasterData(object):
 	#Handles various info from master data.
@@ -160,7 +187,6 @@ class MasterData(object):
 			except:
 				print('Could not load {0} as zlib-compressed data.'.format(datafile_list))
 
-
 	def parse_getMaster(my, infilename='', b64encodeMode=False):
 		with open(infilename, 'rb') as infile:
 			raw_string = zlib.decompress(infile.read()).decode("utf-8")
@@ -172,8 +198,6 @@ class MasterData(object):
 					my.masterJSON.update({key: None})
 				if type(element) is list:
 					my.masterJSON[key] = element
-
-	
 
 	def _extract_section_to_JSON(my, section, rawdata):
 		#Gets all text from one section of the master data.
